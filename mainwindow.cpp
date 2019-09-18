6a955b4b64133a49202221340c7e1241d6207672
ThomHuber <thom-hubers@hotmail.com>
2019-09-18 13:45:03 +0200

    Deleted excess code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -66,55 +66,6 @@ void MainWindow::on_pushButton_File_clicked()
     }
 }
 
-void thresh_callback(int, void* )
-{
-    Mat canny_output;
-    Canny( src_gray, canny_output, thresh, thresh*2 );
-    vector<vector<Point> > contours;
-    findContours( canny_output, contours, RETR_TREE, CHAIN_APPROX_SIMPLE );
-    vector<vector<Point> > contours_poly( contours.size() );
-    vector<Rect> boundRect( contours.size() );
-    vector<Point2f>centers( contours.size() );
-    // vector<float>radius( contours.size() );
-    for( size_t i = 0; i < contours.size(); i++ )
-    {
-        approxPolyDP( contours[i], contours_poly[i], 3, true );
-        boundRect[i] = boundingRect( contours_poly[i] );
-        //minEnclosingCircle( contours_poly[i], centers[i], radius[i] );
-    }
-    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-    for( size_t i = 0; i< contours.size(); i++ )
-    {
-        Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
-        drawContours( drawing, contours_poly, static_cast<int>(i), color );
-        //rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 1 );
-        //circle( drawing, centers[i], static_cast<int>(radius[i]), color, 2 );
-    }
-    imshow( "Contours", drawing );
-}
-
-void step2(Mat src)
-{
-    Mat gray;
-    cvtColor(src,gray,COLOR_BGR2GRAY);
-    threshold(gray,gray,200,255,THRESH_BINARY_INV);
-
-    vector< vector <Point> > contours;
-    vector< Vec4i > hierarchy;
-
-    findContours( gray, contours, hierarchy,RETR_CCOMP, CHAIN_APPROX_SIMPLE );
-
-    for( size_t i = 0; i < contours.size(); i=static_cast<size_t>( hierarchy[i][0]) )
-    {
-        Rect r= boundingRect(contours[i]);
-        rectangle(src,Point(r.x,r.y), Point(r.x+r.width,r.y+r.height), Scalar(0,0,255),1,8,0);
-    }
-
-    imwrite("result2.jpg",src);
-    imshow("step 2 result",src);
-    //waitKey();
-}
-
 void MainWindow::on_pushButton_Webcam_clicked()
 {
     QString info;
