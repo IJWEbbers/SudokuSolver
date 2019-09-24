476f2134fcfb6562f1989b7aa3d5dcde59d87297
IvoEbbers <ijw.ebbers@outlook.com>
2019-09-24 15:02:48 +0200

    added function to separate the grid and assign them to a position in an array

--- detectgrid.cpp
+++ detectgrid.cpp
@@ -3,297 +3,106 @@
 using namespace cv;
 using namespace std;
 
-DetectGrid::DetectGrid()
+//Function to find the Sudoku grid and separate it from the full image
+Mat DetectGrid::findGrid(Mat grayScaleSrc)
 {
+    //clone src
+    Mat srcb = grayScaleSrc.clone();
 
-}
+    //smooth the image
+    Mat smooth;
+    Mat thresholded;
+    GaussianBlur(srcb, smooth, Size(11, 11), 0, 0); //removing noises
+    adaptiveThreshold(smooth, thresholded, 255, ADAPTIVE_THRESH_MEAN_C,THRESH_BINARY_INV, 15, 5);
 
-/**
-* Detect numeric digits in a sudoku grid in img Mat and return Rect instances where they are found
-*/
-vector<Rect> DetectGrid::findDigits(const Mat& img) {
-    vector<Rect> digits;
-    Rect imgRect = Rect(0, 0, img.cols, img.rows);
+    //find contours
+    vector<vector<Point>>contours;
+    findContours(thresholded, contours,RETR_TREE,CHAIN_APPROX_SIMPLE);
 
-    // Find all contours
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-    //findContours( img, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
-    findContours( img, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0) );
-    vector<vector<Point> > contours_poly( contours.size() );
-    vector<Rect> boundRect( contours.size() );
-
-    for( size_t i = 0; i < contours.size(); i++ )
+    //find the biggest contour
+    for (int i = 0; i < static_cast<int>(contours.size()); i++)
     {
-        approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-        boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-
-        if( hierarchy[i][3] < 0 && boundRect[i].height >= MIN_DIGIT_PIXELS) { // find "root" contours
-            double aspectRatio = boundRect[i].height / double(boundRect[i].width);
-            // check reasonable aspect ratio for digits
-            if ((aspectRatio >= 1 && aspectRatio < 3.2)) {
-                int widthToAdd = boundRect[i].height - boundRect[i].width + (2 * DIGIT_PADDING);
-                int pointOffset = int(floor(double(widthToAdd / 2)));
-                boundRect[i] = boundRect[i] - Point(pointOffset, DIGIT_PADDING);
-                boundRect[i] = boundRect[i] + Size(widthToAdd, 2 * DIGIT_PADDING);
-                boundRect[i] &= imgRect;
-
-                // check white/black pixel ratio to avoid accidental noise getting picked up
-                double wbRatio = countNonZero(img(boundRect[i])) / double(boundRect[i].width * boundRect[i].height);
-                if (wbRatio > 0.1 && wbRatio < 0.4) {
-                    digits.push_back(boundRect[i]); // AND boundRect and imgRect to ensure in boundary
-                }
+        area = contourArea(contours[static_cast<unsigned long long>(i)], false);
+        if (area > 50 )
+        {
+            if (area > maxArea)
+            {
+                maxArea = area;
+                maxAreaBlob = i;
             }
         }
     }
+    //get only the corners of the contour
+    double perimeter = arcLength(contours[static_cast<unsigned long long>(maxAreaBlob)], true);
+    approxPolyDP(contours[static_cast<unsigned long long>(maxAreaBlob)], contours[static_cast<unsigned long long>(maxAreaBlob)], 0.1*perimeter, true);
 
-    return digits;
-}
+    //decide in which corner the point belongs
+    double sum = 0; //minimum sum = bottom left, maximum sum = top right
+    double diff; //maximum diff (x - y) = bottom right, minimum diff = top left
 
-/**
-* Attempt to extract and warp sudoku grid
-*/
-void DetectGrid::extractGrid(const Mat& img, Mat& dst, vector<float>& gridPoints, float& scale) {
-    Mat src_gray;
-    blur( img, src_gray, Size(3,3) );
-    Mat canny_output;
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
+    double maxDiff = 0;
+    double minDiff = 0;
+    double maxSum = 0;
+    double minSum = contours[static_cast<unsigned long long>(maxAreaBlob)][0].x + contours[static_cast<unsigned long long>(maxAreaBlob)][0].y;
 
-    int largest_area=0;
-    int largest_contour_index=0;
-    vector<Point> largestContour;
-    Rect bounding_rect;
-
-    Mat denoised;
-    fastNlMeansDenoising(src_gray, denoised, 10);
-
-    adaptiveThreshold(~denoised, src_gray, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 11, -2);
-
-
-    Canny( src_gray, canny_output, CANNY_THRESHOLD, CANNY_THRESHOLD * 2, 3 );
-    findContours( canny_output, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0) );
-    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-
-    for( size_t i = 0; i< contours.size(); i++ )
+    for (int i = 0; i < 4; i++)
     {
-        double a=contourArea( contours[i],false);  //  Find the area of contour
-        if(a>largest_area){
-            largest_area=static_cast<int>(a);
-            largest_contour_index=static_cast<int>(i);                //Store the index of largest contour
-            largestContour = contours[i];
-            bounding_rect=boundingRect(contours[i]); // Find the bounding rectangle for biggest contour
+        sum = contours[static_cast<unsigned long long>(maxAreaBlob)][static_cast<unsigned long long>(i)].x + contours[static_cast<unsigned long long>(maxAreaBlob)][static_cast<unsigned long long>(i)].y;
+        diff = contours[static_cast<unsigned long long>(maxAreaBlob)][static_cast<unsigned long long>(i)].x - contours[static_cast<unsigned long long>(maxAreaBlob)][static_cast<unsigned long long>(i)].y;
+
+        if (diff >= maxDiff)
+        {
+            maxDiff = diff;
+            bottomRight = i;
+        }
+        if (diff <= minDiff)
+        {
+            minDiff = diff;
+            bottomLeft = i;
+        }
+        if (sum >= maxSum)
+        {
+            maxSum = sum;
+            topLeft = i;
+        }
+        if (sum <= minSum)
+        {
+            minSum = sum;
+            topRight = i;
         }
     }
 
-    double area = img.cols * img.rows;
-    if (largest_area < area * MIN_GRID_PCT) {
-        cout << "largest contour area is only " << (largest_area / area) * 100 << "% of source; aborting grid extraction" << endl;
-        img.copyTo(dst);
-        return;
-    }
+    //create the warp arrays
+    Point2f warpIn[4];
+    Point2f warpOut[4];
+    warpIn[0] = contours[static_cast<unsigned long long>(maxAreaBlob)][static_cast<unsigned long long>(topLeft)];
+    warpIn[1] = contours[static_cast<unsigned long long>(maxAreaBlob)][static_cast<unsigned long long>(topRight)];
+    warpIn[2] = contours[static_cast<unsigned long long>(maxAreaBlob)][static_cast<unsigned long long>(bottomRight)];
+    warpIn[3] = contours[static_cast<unsigned long long>(maxAreaBlob)][static_cast<unsigned long long>(bottomLeft)];
+    warpOut[0] = Point2f(450, 450);
+    warpOut[1] = Point2f(0, 0);
+    warpOut[2] = Point2f(450, 0);
+    warpOut[3] = Point2f(0, 450);
 
-    // findCorners
-    if (largest_contour_index < static_cast<int>(contours.size())) {
-        Point2f corners[4];
-        Point2f flatCorners[4];
-        Size sz = findCorners(largestContour, corners);
+    //warp the image
+    Mat wrap; Mat mat;
+    mat = Mat::zeros(grayScaleSrc.size(), grayScaleSrc.type());
+    wrap = getPerspectiveTransform(warpIn, warpOut);
+    warpPerspective(srcb, mat, wrap, Size(450, 450));
 
-
-        // draw contour quadrangle
-        for( int j = 0; j < 4; j++ ) {
-            gridPoints.push_back(corners[j].x * scale);
-            gridPoints.push_back(corners[j].y * scale);
-        }
-        flatCorners[0] = Point2f(0, 0);
-        flatCorners[1] = Point2f(sz.width, 0);
-        flatCorners[2] = Point2f(sz.width, sz.height);
-        flatCorners[3] = Point2f(0, sz.height);
-        Mat lambda = getPerspectiveTransform(corners, flatCorners);
-
-        Mat output;
-        warpPerspective(denoised, output, lambda, sz);
-
-        GaussianBlur(output, dst, Size(0, 0), 3);
-        addWeighted(output, 1.5, dst, -0.5, 0, dst);
-    } else {
-        throw runtime_error("No grid contour found");
-    }
+    return mat;
 }
 
-/**
-* Find corners within the largest contour for use in performing warp transform
-*/
-Size DetectGrid::findCorners(const vector<Point> largestContour, Point2f corners[]) {
-    double dist;
-    double maxDist[4] = {0, 0, 0, 0};
-
-    Moments M = moments(largestContour, true);
-    float cX = float(M.m10 / M.m00);
-    float cY = float(M.m01 / M.m00);
-
-    for (int i = 0; i < 4; i++) {
-        maxDist[i] = 0.0;
-        corners[i] = Point2f(cX, cY);
-    }
-
-    Point2f center(cX, cY);
-
-    // find the most distant point in the contour within each quadrant
-    for (size_t i = 0; i < largestContour.size(); i++) {
-        Point2f p(largestContour[i].x, largestContour[i].y);
-        dist = sqrt(pow(p.x - center.x, 2) + pow(p.y - center.y, 2));
-        // cout << "(" << p.x << "," << p.y << ") is " << dist << " from (" << cX << "," << cY << ")" << endl;
-        if (p.x < cX && p.y < cY && maxDist[0] < dist) {
-            // top left
-            maxDist[0] = dist;
-            corners[0] = p;
-        } else if (p.x > cX && p.y < cY && maxDist[1] < dist) {
-            // top right
-            maxDist[1] = dist;
-            corners[1] = p;
-        } else if (p.x > cX && p.y > cY && maxDist[2] < dist) {
-            // bottom right
-            maxDist[2] = dist;
-            corners[2] = p;
-        } else if (p.x < cX && p.y > cY && maxDist[3] < dist) {
-            // bottom left
-            maxDist[3] = dist;
-            corners[3] = p;
+//Function to assign every box in the grid to a position in an array
+void DetectGrid::splitGrid(Mat grid, Mat gridArray[9][9])
+{
+    Mat smallimage;
+    for (int m=0; m < 450; m += 50)
+    {
+        for (int n = 0; n < 450; n += 50)
+        {
+            smallimage = Mat(grid, Rect(n, m, 50, 50));
+            gridArray[n/50][m/50] = smallimage;
         }
     }
-
-    // determine the dimensions that we should warp this contour to
-    double widthTop = sqrt(pow(corners[0].x - corners[1].x, 2) + pow(corners[0].y - corners[1].y, 2));
-    double widthBottom = sqrt(pow(corners[2].x - corners[3].x, 2) + pow(corners[2].y - corners[3].y, 2));
-
-    double heightLeft = sqrt(pow(corners[0].x - corners[3].x, 2) + pow(corners[0].y - corners[3].y, 2));
-    double heightRight = sqrt(pow(corners[1].x - corners[2].x, 2) + pow(corners[1].y - corners[2].y, 2));
-
-
-    return Size(static_cast<int>(max(widthTop, widthBottom)), static_cast<int>(max(heightLeft, heightRight)));
-}
-
-/**
-* Detect horizontal/vertical lines of a sudoku grid in img Mat and copy expanded lines to dst Mat
-*/
-void extractLines(const Mat& img, Mat& dst, bool horizontal) {
-    // Clone the source image
-    Mat clone = img.clone();
-
-    LineTestFn lineTest;
-    ExpandRectFn expandRect;
-    Size size;
-
-    // Setup correct structure size, line test and rect expansion for horizontal vs. vertical
-    if (horizontal) {
-        size = Size(img.cols / 9, 1);
-        lineTest = [](Rect& rect, Mat& mat) { return rect.height / double(mat.rows) < 0.05 && rect.width / double(mat.cols) > 0.111; };
-        expandRect = [](Rect& rect, Mat& mat) {
-            Rect expanded = rect;
-            if (expanded.y > 1) { expanded.y -= 2; }
-
-            if (expanded.y + expanded.height < mat.rows) {
-                expanded.height += min(4, mat.rows - expanded.y - expanded.height);
-            }
-            expanded.x = 0;
-            expanded.width = mat.cols;
-            return expanded;
-        };
-    } else {
-        size = Size(1, img.rows / 9);
-        lineTest = [](Rect& rect, Mat& mat) { return rect.width / double(mat.cols) < 0.05 && rect.height / double(mat.rows) > 0.111; };
-        expandRect = [](Rect& rect, Mat& mat) {
-            Rect expanded = rect;
-            if (expanded.x > 1) { expanded.x -= 2; }
-
-            if (expanded.x + expanded.width < mat.cols) {
-                expanded.width += min(4, mat.cols - expanded.x - expanded.width);
-            }
-            expanded.y = 0;
-            expanded.height = mat.rows;
-            return expanded;
-        };
-    }
-    // Create structure element for extracting lines through morphology operations
-    Mat structure = getStructuringElement(MORPH_RECT, size);
-
-    // Apply morphology operations
-    erode(clone, clone, structure, Point(-1, -1));
-    dilate(clone, clone, structure, Point(-1, -1));
-
-    // Find all contours
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-    findContours( clone, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
-    vector<vector<Point> > contours_poly( contours.size() );
-    vector<Rect> boundRect( contours.size() );
-
-    // Mark contours which pass line test in the destination image
-    for( size_t i = 0; i < contours.size(); i++ )
-    {
-        approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-        boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-        if (lineTest(boundRect[i], clone)) {
-            Rect expanded = expandRect(boundRect[i], clone);
-            dst(expanded) |= 255; // set the expanded rect to white
-        }
-    }
-}
-
-/**
-* Detect Sudoku board and digits in the "raw" Mat
-*/
-vector<Rect> DetectGrid::FindDigitRects(const Mat& raw, Mat& cleaned, vector<float>& gridPoints, float &scale) {
-    // Check if image is loaded fine
-    if(!raw.data)
-        cerr << "Problem loading image!!!" << endl;
-
-    Mat src;
-    raw.copyTo(src);
-
-    // Transform source image to gray if it is not
-    Mat gray;
-    if (src.channels() == 3)
-    {
-        cvtColor(src, gray, COLOR_BGR2GRAY);
-    }
-    else
-    {
-        gray = src;
-    }
-
-    // make sure image is a reasonable size
-    if(gray.rows > MAX_PUZZLE_SIZE || gray.cols > MAX_PUZZLE_SIZE) {
-        scale = max(gray.rows, gray.cols) / float(MAX_PUZZLE_SIZE);
-        resize(gray, gray, Size(static_cast<int>(gray.cols / scale), static_cast<int>(gray.rows / scale)), 0, 0, INTER_AREA);
-    } else if (gray.rows < MIN_PUZZLE_SIZE || gray.cols < MIN_PUZZLE_SIZE) {
-        scale = min(gray.rows, gray.cols) / float(MAX_PUZZLE_SIZE);
-        resize(gray, gray, Size(static_cast<int>(gray.cols / scale), static_cast<int>(gray.rows / scale)), 0, 0, INTER_CUBIC);
-    }
-
-    Mat grid = Mat::zeros( gray.size(), gray.type() );
-    extractGrid(gray, grid, gridPoints, scale);
-
-    // Apply adaptiveThreshold at the bitwise_not of gray, notice the ~ symbol
-    Mat bw;
-    adaptiveThreshold(~grid, bw, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 11, -2);
-
-    Mat lines = Mat::zeros( bw.size(), bw.type() );
-    extractLines(bw, lines, true);
-    extractLines(bw, lines, false);
-
-    imshow("lines", lines);
-
-    // subtract grid lines from the black/white image
-    // so they don't interfere with digit detection
-    Mat clean = bw - lines;
-    blur(clean, clean, Size(1, 1));
-
-    // find digits
-    vector<Rect> digits = findDigits(clean);
-
-    clean.copyTo(cleaned);
-
-    return digits;
 }
--- detectgrid.h
+++ detectgrid.h
@@ -1,9 +1,12 @@
 #ifndef DETECTGRID_H
 #define DETECTGRID_H
-
+#include <stdio.h>
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/opencv.hpp"
+
+#define UNASSIGNED 0 // UNASSIGNED is used for empty cells in sudoku
+#define N 9 // N is used for size of Sudoku grid. Size will be NxN
 
 using namespace cv;
 using namespace std;
@@ -13,18 +16,16 @@ using ExpandRectFn = function<Rect(Rect&, Mat&)>;
 class DetectGrid
 {
 private:
-    const int DIGIT_PADDING = 3;
-    const int MIN_DIGIT_PIXELS = 20;
-    const int CANNY_THRESHOLD = 65;
-    const double MIN_GRID_PCT = 0.3;
-    const int MIN_PUZZLE_SIZE = 325;
-    const int MAX_PUZZLE_SIZE = 900;
+    double area;
+    double maxArea = 0;
+    int maxAreaBlob;
+    int bottomRight = 0;
+    int bottomLeft = 0;
+    int topLeft = 0;
+    int topRight = 0;
 public:
-    DetectGrid();
-    void extractGrid(const Mat& img, Mat& dst, vector<float>& gridPoints, float& scale);
-    Size findCorners(const vector<Point> largestContour, Point2f corners[]);
-    vector<Rect> FindDigitRects(const Mat& raw, Mat& cleaned, vector<float>& gridPoints, float &scale);
-    vector<Rect> findDigits(const Mat& img);
+    Mat findGrid(Mat grayScaleSrc);
+    void splitGrid(Mat grid, Mat gridArray[9][9]);
 };
 
 #endif // DETECTGRID_H
